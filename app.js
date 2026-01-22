ec6b84bb44be89259d5f07c0ef54683f420ec881
nicolas <nicolasbernard19@gmail.com>
2026-01-22 10:43:17 +0000

    setup cookie click event

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
 console.log("Cookie Clicker Started!");
 
-// 1. get cookie img and setup on click event
+// Global variables
 
+// 1. get cookie img and setup on click event
+const cookieImg = document.getElementById("cookieImg");
+cookieImg.addEventListener("click", (event)=> {
+    console.log("The cookie has been clicked!")
+});
 
 // 2. get cookie bank and total cookies by id selector
 
