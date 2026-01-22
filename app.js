4fe3e377f2ac23b053be9037da721c97f54ab6a1
nicolas <nicolasbernard19@gmail.com>
2026-01-22 11:42:05 +0000

    add cookieTotal & bank Globals, & make ex shop list

--- app.js
+++ app.js
@@ -2,6 +2,26 @@
 console.log("Cookie Clicker Started!");
 
 // Global variables
+let cookies = 0; // later get this value from local storage
+const CookieTotal = document.getElementById("cookieTotal");
+const cookieBank = document.getElementById("cookieBank");
+
+// Cost of things in the shop
+// fetch the items from the shop
+
+
+let costs = {
+    'rollingPin': 10,
+    'cookieMoulds': 100
+}
+
+let listCost = [{
+    name: 'rollingPin',
+    cost: 10
+}]
+
+console.log("rollingPin cost: ", costs.rollingPin);
+console.log("cookieMoulds cost: ", costs.cookieMoulds);
 
 // 1. get cookie img and setup on click event
 const cookieImg = document.getElementById("cookieImg");
